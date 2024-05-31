df2540aece5ae8232d4648b3e3db4a1c5afe6a2a
= <santoshgiri2345@gmail.com>
2024-05-31 23:00:18 +0545

    added jwt

--- chat-server/controllers/authController.js
+++ chat-server/controllers/authController.js
@@ -12,7 +12,7 @@ const resetPassword = require("../Templates/Mail/resetPassword");
 const { promisify } = require("util");
 const catchAsync = require("../utils/catchAsync");
 
-// this function will return you jwt token
+// this function will return you jwt token with the user id
 const signToken = (userId) => jwt.sign({ userId }, process.env.JWT_SECRET);
 
 // Register New User or Update Existing User
