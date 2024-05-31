eb4357fe952b6f20be1e9eba1d97e72c610f6ab2
= <santoshgiri2345@gmail.com>
2024-05-31 13:30:06 +0545

    added necessary comments on auth route file

--- chat-server/routes/auth.js
+++ chat-server/routes/auth.js
@@ -2,11 +2,13 @@ const router = require("express").Router();
 
 const authController = require("../controllers/authController");
 
-router.post("/login", authController.login);
-router.post("/register", authController.register, authController.sendOTP);
-router.post("/verify", authController.verifyOTP);
-router.post("/send-otp", authController.sendOTP);
-router.post("/forgot-password", authController.forgotPassword);
-router.post("/reset-password", authController.resetPassword);
+
+
+router.post("/login", authController.login);    // this is the login route where the user will send the request to login and the authController.login will handle the request
+router.post("/register", authController.register, authController.sendOTP);  // this is the register route where the user will send the request to register and the authController.register will handle the request
+router.post("/verify", authController.verifyOTP);   // this is the verify route where the user will send the request to verify the otp and the authController.verifyOTP will handle the request
+router.post("/send-otp", authController.sendOTP);   // this is the send-otp route where the user will send the request to send the otp and the authController.sendOTP will handle the request
+router.post("/forgot-password", authController.forgotPassword); // this is the forgot-password route where the user will send the request to forgot the password and the authController.forgotPassword will handle the request
+router.post("/reset-password", authController.resetPassword);   // this is the reset-password route where the user will send the request to reset the password and the authController.resetPassword will handle the request
 
 module.exports = router;
