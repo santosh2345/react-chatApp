bc12a55cf49a04b88c8ff4ba480c88126d4fb4ab
= <santoshgiri2345@gmail.com>
2024-05-31 23:02:58 +0545

    added jwt

--- chat-server/controllers/authController.js
+++ chat-server/controllers/authController.js
@@ -298,7 +298,7 @@ exports.resetPassword = catchAsync(async (req, res, next) => {
   user.passwordResetExpires = undefined;
   await user.save();
 
-  // 3) Update changedPasswordAt property for the user
+  // 3) Update changedPasswordAt property for the user DONE
   // 4) Log the user in, send JWT
   const token = signToken(user._id);
 
