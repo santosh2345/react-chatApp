0b3d590b694e4f0e93ac5d4774ce3a175f0f5cff
= <santoshgiri2345@gmail.com>
2024-06-01 22:00:01 +0545

    added comments

--- chat-server/routes/user.js
+++ chat-server/routes/user.js
@@ -10,7 +10,7 @@ router.post(
 );
 router.get("/get-call-logs", authController.protect, userController.getCallLogs);  // this is the get-call-logs route where the user will send the request to get the call logs and the userController.getCallLogs will handle the request
 router.get("/get-me", authController.protect, userController.getMe);  // this is the get-me route where the user will send the request to get the user details and the userController.getMe will handle the request
-router.patch("/update-me", authController.protect, userController.updateMe);
+router.patch("/update-me", authController.protect, userController.updateMe);  // this is the update-me route where the user will send the request to update the user details and the userController.updateMe will handle the request
 router.get("/get-all-verified-users", authController.protect, userController.getAllVerifiedUsers);
 router.get("/get-users", authController.protect, userController.getUsers);
 router.get("/get-requests", authController.protect, userController.getRequests);
