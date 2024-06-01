ade62c7a048a95877ad6d95a90ac82b889514c1e
= <santoshgiri2345@gmail.com>
2024-06-01 22:00:43 +0545

    added comments

--- chat-server/routes/user.js
+++ chat-server/routes/user.js
@@ -14,7 +14,7 @@ router.patch("/update-me", authController.protect, userController.updateMe);  //
 router.get("/get-all-verified-users", authController.protect, userController.getAllVerifiedUsers);  // this is the get-all-verified-users route where the user will send the request to get all the verified users and the userController.getAllVerifiedUsers will handle the request
 router.get("/get-users", authController.protect, userController.getUsers);  // this is the get-users route where the user will send the request to get the users and the userController.getUsers will handle the request
 router.get("/get-requests", authController.protect, userController.getRequests);  // this is the get-requests route where the user will send the request to get the requests and the userController.getRequests will handle the request
-router.get("/get-friends", authController.protect, userController.getFriends);
+router.get("/get-friends", authController.protect, userController.getFriends);  // this is the get-friends route where the user will send the request to get the friends and the userController.getFriends will handle the request
 
 router.post("/start-audio-call", authController.protect, userController.startAudioCall);
 router.post("/start-video-call", authController.protect, userController.startVideoCall);
