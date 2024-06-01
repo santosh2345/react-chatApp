7fa3d2829455dc65d51c1c475a57fb771694a8e7
= <santoshgiri2345@gmail.com>
2024-06-01 22:01:36 +0545

    added comments

--- chat-server/routes/user.js
+++ chat-server/routes/user.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 
-const userController = require("../controllers/userController");
+const userController = require("../controllers/userController");  // this is the userController file where the userController functions are defined
 const authController = require("../controllers/authController");
 
 // this is the get-all-users route where the user will send the request to get all the users and the userController.getAllUsers will handle the request
