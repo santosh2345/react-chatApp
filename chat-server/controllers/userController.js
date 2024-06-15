ff70316f97802d2772c494b311ac08392eaf8e10
= <santoshgiri2345@gmail.com>
2024-06-15 22:17:28 +0545

    reviewing

--- chat-server/controllers/userController.js
+++ chat-server/controllers/userController.js
@@ -42,18 +42,7 @@ exports.updateMe = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.getUsers = catchAsync(async (req, res, next) => {
-  const all_users = await User.find({
-    verified: true,
-  }).select("firstName lastName _id");
 
-  const this_user = req.user;
-
-  const remaining_users = all_users.filter(
-    (user) =>
-      !this_user.friends.includes(user._id) &&
-      user._id.toString() !== req.user._id.toString()
-  );
 
   res.status(200).json({
     status: "success",
