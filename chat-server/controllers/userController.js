2157dbe0d166f5053bbbb0049e1888f8fcb2e2b5
= <santoshgiri2345@gmail.com>
2024-06-15 22:17:20 +0545

    reviewing

--- chat-server/controllers/userController.js
+++ chat-server/controllers/userController.js
@@ -25,7 +25,22 @@ exports.getMe = catchAsync(async (req, res, next) => {
 });
 
 
+exports.updateMe = catchAsync(async (req, res, next) => {
+  const filteredBody = filterObj(
+    req.body,
+    "firstName",
+    "lastName",
+    "about",
+    "avatar"
+  );
+  const userDoc = await User.findByIdAndUpdate(req.user._id, filteredBody);
 
+  res.status(200).json({
+    status: "success",
+    data: userDoc,
+    message: "User Updated successfully",
+  });
+});
 
 exports.getUsers = catchAsync(async (req, res, next) => {
   const all_users = await User.find({
