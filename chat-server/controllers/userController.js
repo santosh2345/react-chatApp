ff9fec28594d4298959e90ccc191653ebf3c1511
= <santoshgiri2345@gmail.com>
2024-06-16 21:43:41 +0545

    reviewing

--- chat-server/controllers/userController.js
+++ chat-server/controllers/userController.js
@@ -42,7 +42,25 @@ exports.updateMe = catchAsync(async (req, res, next) => {
   });
 });
 
+exports.getUsers = catchAsync(async (req, res, next) => {
+  const all_users = await User.find({
+    verified: true,
+  }).select("firstName lastName _id");
 
+  const this_user = req.user;
+
+  const remaining_users = all_users.filter(
+    (user) =>
+      !this_user.friends.includes(user._id) &&
+      user._id.toString() !== req.user._id.toString()
+  );
+
+  res.status(200).json({
+    status: "success",
+    data: remaining_users,
+    message: "Users found successfully!",
+  });
+});
 
 exports.getAllVerifiedUsers = catchAsync(async (req, res, next) => {
   const all_users = await User.find({
