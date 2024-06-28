1e39d23fb0cd1df649f7e88271993c6b703663c0
= <santoshgiri2345@gmail.com>
2024-06-28 15:32:12 +0545

    reviewing and docs

--- chat-server/routes/index.js
+++ chat-server/routes/index.js
@@ -2,10 +2,10 @@ const router = require("express").Router(); // this is the router of the express
 const authRoute = require("./auth");    // this is the auth route where all the auth routes are defined
 const userRoute = require("./user");    // this is the user route where all the user routes are defined
 
-router.use("/auth", authRoute); // this will use the auth route and all the routes are defined in the auth route
+router.use("/auth", authRoute); // this will use the auth route and all the routes are defined in the auth route  
 router.use("/user", userRoute); // this will use the user route and all the routes are defined in the user route
 
-module.exports = router;
+module.exports = router;    // this will export the router
 
 
 
